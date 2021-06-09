ea304e2b78c0f6e648cc9a2d4aedbef193cf7f69
Vcainzos <git-commit-address>
2021-06-09 16:05:07 +0200

    restore docs

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Víctor Caínzos López"
 author = "Víctor Caínzos López"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "1.0"
 
 
 # -- General configuration ---------------------------------------------------
